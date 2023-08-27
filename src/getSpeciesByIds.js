e7679a65cc190bd58d258f93016878f65aef0190
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-27 19:33:49 -0300

    Iniciando projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0);
+  return [];
 };
 
 module.exports = getSpeciesByIds;
