395c62374444accc8f99942ce5769ccde60407b4
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-28 00:08:41 -0300

    Requisito 2, retirando console.log do requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const ageSpecie = species.find((specie) => specie.name.includes(animal));
+
+  if (ageSpecie === age) return true;
+
+  return ageSpecie.residents.every((resident) => resident.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,6 +9,4 @@ const getSpeciesByIds = (...ids) => {
   return speciesId;
 };
 
-//console.log(getSpeciesByIds('ef3778eb-2844-4c7c-b66c-f432073e1c6b', '01422318-ca2d-46b8-b66c-3e9e188244ed'));
-
 module.exports = getSpeciesByIds;
