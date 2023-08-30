9fd4c1b893fd03a05006a44b7df4e0477c781d71
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-29 22:25:59 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
-};
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
+const isManager = (id) => managers.includes(id);
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const related = data.employees
+    .filter((worker) => worker.managers.includes(managerId))
+    .map((worker) => `${worker.firstName} ${worker.lastName}`);
+
+  return related;
 };
 
 module.exports = { isManager, getRelatedEmployees };
