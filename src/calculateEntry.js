9796bbb793bd574fc5ee798b334475fe8f1bce12
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-31 15:20:54 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const entrantsAge = { child: 0, adult: 0, senior: 0,
+  };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      entrantsAge.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      entrantsAge.adult += 1;
+    } else {
+      entrantsAge.senior += 1;
+    }
+  });
+  return entrantsAge;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const totalEntrantsAge = countEntrants(entrants);
+  const { child: childPrice, adult: adultPrice, senior: seniorPrice } = data.prices;
+
+  const childTotalPrice = totalEntrantsAge.child * childPrice;
+  const adultTotalPrice = totalEntrantsAge.adult * adultPrice;
+  const seniorTotalPrice = totalEntrantsAge.senior * seniorPrice;
+
+  return +(childTotalPrice + adultTotalPrice + seniorTotalPrice).toFixed(2);
 };
 
 module.exports = { calculateEntry, countEntrants };
