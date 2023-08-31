7ec6f105557cd1583ec97a3da9b0549708113319
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-31 14:44:59 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const animalsInGeral = data.species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
+
+const lookForAnimals = (animal) => data.species.find(({ name }) => animal.species === name);
+
+const lookForAnimalsBySex = (animal) => data.species
+  .find((specie) => animal.species === specie.name).residents
+  .filter((resident) => animal.sex === resident.sex).length;
+
+const countAnimals = (obj = {}) => {
+  if (obj.sex && obj.species) {
+    return lookForAnimalsBySex(obj);
+  }
+  if (obj.species) {
+    return lookForAnimals(obj).residents.length;
+  }
+  return animalsInGeral;
 };
 
 module.exports = countAnimals;
