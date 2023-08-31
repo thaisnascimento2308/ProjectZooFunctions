dde6083833d4c605d80b98580f1b22faa73ea253
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-31 20:01:17 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const { employees, species } = data;
+
+const employeesData = (employee) => {
+  const employeeInf = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor.map((specieId) => {
+      const specie = species.find((speciess) => speciess.id === specieId);
+      return specie.name;
+    }),
+    locations: employee.responsibleFor.map((specieId) => {
+      const specie = species.find((speciess) => speciess.id === specieId);
+      return specie.location;
+    }),
+  };
+
+  return employeeInf;
+};
+const getEmployeesCoverage = (object) => {
+  if (!object) {
+    return employees.map((info) => employeesData(info));
+  }
+  const worker = employees.find((info) => [info.firstName, info.lastName]
+    .includes(object.name) || [info.id]
+    .includes(object.id));
+  if (worker) {
+    return employeesData(worker);
+  }
+  throw new Error('Informações inválidas');
 };
 
 module.exports = getEmployeesCoverage;
