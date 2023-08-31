49b74297e74f8bb55851075588fc2ce7e2fe556d
Thais Nascimento <thaisnascimento2308@gmail.com>
2023-08-31 17:45:38 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
+
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  if (!id) {
+    return [];
+  }
+
+  const foundEmployee = employees.find((employee) => employee.id === id);
+  const foundAnimal = foundEmployee.responsibleFor[0];
+  const gAnimals = species.find((animal) => animal.id === foundAnimal);
+  const animalOlder = gAnimals.residents
+    .reduce((older, current) => (current.age > older.age ? current : older), gAnimals.residents[0]);
+
+  return Object.values(animalOlder);
 };
 
 module.exports = getOldestFromFirstSpecies;
